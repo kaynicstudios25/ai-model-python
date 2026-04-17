8e0182441393538113e91fe5f657daeb917dcabb
Kaylyn Groom <kngroom@uab.edu>
2026-04-17 21:38:36 +0000

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()  # Load from .env file
+
+client = OpenAI(
+    api_key=os.getenv('AI_KEY'),
+    base_url=os.getenv('AI_URL')
+)
+
+messages = [{
+    "role": "user", 
+    "content": "Write a one-sentence bedtime story about a mermaid."
+}]
+
+response = client.chat.completions.create(
+    model=os.getenv('AI_MODEL'),
+    messages=messages
+)
+
+print(response.choices[0].message.content)
